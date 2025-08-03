16114fabeb0008f68c2b6ba5465fa464be1eb6fb
Okami-4942 <rio.ksmt@gmail.com>
2025-08-03 10:54:05 +0900

    shop置き換え済

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xeeeeee);
 
 const camera = new THREE.PerspectiveCamera(
-  75, window.innerWidth / window.innerHeight, 0.1, 1000
+  65, window.innerWidth / window.innerHeight, 0.1, 1000
 );
 camera.position.set(0,3,10);
 
